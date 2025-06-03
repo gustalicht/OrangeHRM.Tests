61aab148a17429d1da6d870ac6b6fa641821f21f
gustalicht <90413439+gustalicht@users.noreply.github.com>
2025-06-02 22:45:23 -0300

    inicial

---
+++ PlaywrightDriver.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace OrangeHRM.Tests.Drivers;
+
+public static class PlaywrightDriver
+{
+    public static async Task<IBrowser> GetBrowserAsync()
+    {
+        var playwright = await Playwright.CreateAsync();
+        return await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false // true para rodar em modo invisível
+        });
+    }
+}
